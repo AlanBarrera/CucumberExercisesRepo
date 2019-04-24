4e499b777ae7e14a49b36b512b1be80f0f8cfcb2
AlanBarrera <alan.barrera@4thsource.com>
2019-04-24 12:41:54 -0500

    Rename TestRunner class.

--- CucumberExercises/src/testRunners/TestRunner_All.java
+++ CucumberExercises/src/testRunners/TestRunner_All.java
@@ -7,6 +7,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "resources/features", glue = "stepDefinitions")
 
-public class TestRunner_Register {
+public class TestRunner_All {
 
 }
